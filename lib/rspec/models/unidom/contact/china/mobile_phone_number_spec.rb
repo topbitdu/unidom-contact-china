5c95ab12a908adf7477f13b6325f2f735d9875d1
Topbit Du <topbit.du@gmail.com>
2017-02-15 09:55:50 +0800

    1, Improve the Mobile Phone Number spec for the validations on the #phone_number attribute.

--- lib/rspec/models/unidom/contact/china/mobile_phone_number_spec.rb
+++ lib/rspec/models/unidom/contact/china/mobile_phone_number_spec.rb
@@ -14,6 +14,35 @@ describe Unidom::Contact::China::MobilePhoneNumber, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    it_behaves_like 'validates', model_attributes, :phone_number,
+      {                              } => 0,
+      { phone_number: nil            } => 4,
+      { phone_number: ''             } => 4,
+      { phone_number: 'A'            } => 3,
+      { phone_number: '1'            } => 2,
+      { phone_number: '1391234567'   } => 2,
+      { phone_number: 1391234567     } => 2,
+      { phone_number: '-1391234567'  } => 1,
+      { phone_number: -1391234567    } => 1,
+      { phone_number: '13912345678'  } => 0,
+      { phone_number: 13912345678    } => 0,
+      { phone_number: -13912345678   } => 2,
+      { phone_number: '-13912345678' } => 2,
+      { phone_number: '139123456789' } => 2,
+      { phone_number: '1391234567.0' } => 2,
+      { phone_number: '1391234567.8' } => 2,
+      { phone_number: '10912345678'  } => 1,
+      { phone_number: '11912345678'  } => 1,
+      { phone_number: '12912345678'  } => 1,
+      { phone_number: '13912345678'  } => 0,
+      { phone_number: '14912345678'  } => 0,
+      { phone_number: '15912345678'  } => 0,
+      { phone_number: '16912345678'  } => 0,
+      { phone_number: '17912345678'  } => 0,
+      { phone_number: '18912345678'  } => 0,
+      { phone_number: '19912345678'  } => 0,
+      { phone_number: 'A'*11         } => 2
+
   end
 
 end
